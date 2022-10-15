2ce5e55a4576fe8b533fa3cff0fd185991fdce05
ksigei <sigeikiprono4@gmail.com>
2022-10-15 21:55:19 +0300

    Add splashs screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
 
   protected
 
@@ -11,5 +10,13 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_for(_resources)
     redirect_to new_user_session
+  end
+
+  def after_sign_in_path_for(_resources)
+    groups_path
+  end
+
+  def after_sign_up_path_for(_resources)
+    groups_path
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    # redirect_to groups_path if user_signed_in?
-
+    redirect_to groups_path if user_signed_in?
   end
 end
