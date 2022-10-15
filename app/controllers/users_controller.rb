2fec1117da1f233ee3233d306e3d283198896242
ksigei <sigeikiprono4@gmail.com>
2022-10-15 15:22:28 +0300

    Working links to entities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_user
-    # c
+    @user = User.find(current_user.id)
   end
 
   # Only allow a list of trusted parameters through.
